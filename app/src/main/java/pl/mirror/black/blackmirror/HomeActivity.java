0a83b440e1af632a18511b86a43dffcd51c70ec9
Przemek <pszem.szym@gmail.com>
2017-04-02 14:39:16 +0200

    Init android things project

---
+++ app/src/main/java/pl/mirror/black/blackmirror/HomeActivity.java
@@ -0,0 +1,13 @@
+package pl.mirror.black.blackmirror;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
